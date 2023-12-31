a67093b834f68e6489ac39e11595cd6670f0e5c2
María Imbert <imbert.maria@hotmail.com>
2023-12-31 14:15:23 -0300

    add async await on fetch data

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,77 @@
 <script setup>
-import { ref, computed} from "vue";
+import { ref, computed, onMounted } from "vue";
 import BlogPost from "./components/BlogPost.vue";
-import PaginatePost from "./components/PaginatePost.vue"
+import PaginatePost from "./components/PaginatePost.vue";
+import LoadingSpinner from "./components/LoadingSpinner.vue";
 
-const posts = ref([])
+const posts = ref([]);
 const favorito = ref("");
 
 const postXpage = 5;
 const inicio = ref(0);
-const fin = ref(postXpage)
+const fin = ref(postXpage);
+const loading = ref(true);
 
-fetch("https://jsonplaceholder.typicode.com/posts")
-    .then(res => res.json())
-    .then(data => posts.value = data)
+/*Utilizando Fetch*/
+// fetch("https://jsonplaceholder.typicode.com/posts")
+//     .then((res) => res.json())
+//     .then((data) => (posts.value = data))
+//     .catch((e) => console.log(e))
+//     .finally(() => (loading.value = false));
 
+/*Utilizando async await*/
+
+
+const fetchData = async() => {
+    try {
+        const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+        posts.value = await res.json();
+    } catch (error) {
+        console.log(error);
+    } finally {
+        setTimeout(() => {
+            loading.value = false;
+        }, 2000);
+    }
+}
+
+fetchData()
 
 const next = () => {
-    inicio.value += postXpage
-    fin.value +=  postXpage
-}
+    inicio.value += postXpage;
+    fin.value += postXpage;
+};
 
 const prev = () => {
-    inicio.value -= postXpage
-    fin.value -= postXpage
-}
+    inicio.value -= postXpage;
+    fin.value -= postXpage;
+};
 
-const maxLength = computed(()=> {
-    return posts.value.length
-})
+const maxLength = computed(() => {
+    return posts.value.length;
+});
 </script>
 
 <template>
     <div class="app-container">
-        <div class="post-container">
+        <LoadingSpinner v-if="loading" />
+        <div class="post-container" v-else>
             <h1>Mi post favorito: {{ favorito }}</h1>
-            <PaginatePost @next="next" @prev="prev" :inicio="inicio" :fin="fin" :maxLength="maxLength"></PaginatePost>
-            <BlogPost v-for="post in posts.slice(inicio, fin)"
-            :key = post.id
-            :id = post.id
-            :title = post.title
-            :body = post.body
-            > </BlogPost>
+            <PaginatePost
+                @next="next"
+                @prev="prev"
+                :inicio="inicio"
+                :fin="fin"
+                :maxLength="maxLength"
+            ></PaginatePost>
+            <BlogPost
+                v-for="post in posts.slice(inicio, fin)"
+                :key="post.id"
+                :id="post.id"
+                :title="post.title"
+                :body="post.body"
+            >
+            </BlogPost>
         </div>
     </div>
 </template>
@@ -48,6 +79,7 @@ const maxLength = computed(()=> {
 <style>
 .app-container {
     width: 100%;
+    height: 100vh;
     display: flex;
     justify-content: center;
 }
---
+++ src/components/LoadingSpinner.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="loading-container">
+        <v-progress-circular color="primary" model-value="20" :size="60" indeterminate></v-progress-circular>
+    <p>Loading...</p>
+    </div>
+
+</template>
+
+<style>
+.loading-container {
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   justify-content: center;
+}
+</style>
